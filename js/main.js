4c47e7b9ba1f67d10188c5dfe19760f020da94af
Hesham <hesham2go@outlook.com>
2021-08-07 15:36:26 -0400

    inital commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+let result = 0 
+
+let result_div = document.getElementById("result")
+let add_btn = document.getElementById("add")
+let sub_btn = document.getElementById("subtract")
+
+add_btn.addEventListener("click", add); 
+sub_btn.addEventListener("click", sub)
+
+
+function add() {
+    let value_input = parseInt(document.getElementById("numb").value)
+    result += value_input
+    console.log(result)
+    result_div.innerText = (result)
+    document.getElementById("numb").value = ""
+}
+
+function sub() {
+    let value_input = parseInt(document.getElementById("numb").value)
+    result -= value_input
+    if(result < 0){result_div.style.color = "red"}
+    result_div.innerText = (result)
+    document.getElementById("numb").value = ""
+    
+}
